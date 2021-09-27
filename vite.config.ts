7c5b9966dfba5a3f7c2964bec6de98ecabe57a45
叶苏雄 <xiesuxiong@xiesuxiongdeMacBook-Pro.local>
2021-09-27 23:31:30 +0800

    初步配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const path = require('path');
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  root: __dirname,
+  plugins: [vue()],
+  resolve: {
+    alias: [{
+      find: '@',
+      replacement: path.resolve(__dirname, './src')
+    }]
+  }
 })
