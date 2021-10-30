2e588f3608bf79069ed666356fd44686d86a8324
YeSuX <351490949@qq.com>
2021-10-30 14:21:48 +0800

    storybool 构建问题解决

--- .storybook/base/config.ts
+++ .storybook/base/config.ts
@@ -1,19 +1,33 @@
+const path = require('path');
+const _ = require('lodash');
+
+function resolve(...dirs) {
+    return path.join(__dirname, '../..', ...dirs);
+}
+
 module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
-  "core": {
-    "builder": "storybook-builder-vite"
-  },
-  viteFinal: async (config)=>{
-    config.css = {
-      preprocessorOptions: {
-        scss: {
-          javascriptEnabled: true,
-        },
-      },
-    }
+  // "core": {
+  //   "builder": "storybook-builder-vite"
+  // },
+  webpackFinal: async (config)=>{
+    const rules = [];
+    rules.push({
+      test: /\.s(a|c)ss$/,
+      include: [resolve('packages')],
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    })
+    config.module.rules.push(...rules);
+    // config.css = {
+    //   preprocessorOptions: {
+    //     scss: {
+    //       javascriptEnabled: true,
+    //     },
+    //   },
+    // }
     return config;
   }
 }
