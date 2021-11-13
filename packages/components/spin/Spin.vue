b89a80fc4831385c392cbd7dddb2fff731986ad4
YeSuX <351490949@qq.com>
2021-11-13 15:15:34 +0800

    spin completed

---
+++ packages/components/spin/Spin.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    v-if="loading"
+    class="sx-spin"
+    :class="[size ? `sx-spin-${size}` : null, tip ? 'sx-spin-block' : null]"
+  >
+    <div class="sx-spin-wrapper">
+      <sx-Icon :name="indicator" spin></sx-Icon>
+      <div v-if="tip">{{ tip }}</div>
+    </div>
+    <div class="sx-spin-children">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import sxIcon from "../icon/Icon.vue";
+import { props } from "./props";
+
+export default defineComponent({
+  name: "SxSpin",
+  props,
+  components: {
+    sxIcon,
+  },
+  setup(props) {
+    const loading = ref(false);
+    setTimeout(() => {
+      loading.value = true;
+    }, props.delay);
+    return { loading };
+  },
+});
+</script>
+
+<style lang="scss">
+@import "./Spin.scss";
+</style>
---
+++ packages/components/spin/props.ts
@@ -0,0 +1,24 @@
+import { PropType } from "@vue/runtime-core";
+
+// const sizeValidator = (size) => {
+//   return ['small' || 'middle' || 'large'].includes(size);
+// }
+
+export const props = {
+  size: {
+    type: String as PropType<'small' | 'middle' | 'large'>,
+    default: 'middle',
+    // validator: sizeValidator
+  },
+  tip: {
+    type: String
+  },
+  indicator: {
+    type: String,
+    default: 'spin'
+  },
+  delay: {
+    type: Number,
+    default: 0
+  }
+}
